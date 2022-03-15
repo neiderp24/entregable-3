7e19a43024e7fcc3881dd943858f2c7c25bbcd92
neiderp24 <neiderpb@gmail.com>
2022-03-14 19:03:24 -0500

    final

---
+++ src/components/ResidentsList.js
@@ -0,0 +1,20 @@
+import ResidentsInfo from './ResidentsInfo';
+
+const ResidentsList = ({characters}) => {
+   
+console.log(characters)
+    
+
+    return (
+        <div>
+             {characters?.map((character)=>
+             <ResidentsInfo
+                characterUrl={character}
+                key={character}
+              />     
+             )}
+        </div>
+    );
+};
+
+export default ResidentsList;
